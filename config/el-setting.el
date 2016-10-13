992bd4a8d1d78e7dbad57315abfc09161e524060
Haoran Feng <haoran.feng@alo7.com>
2016-10-13 13:15:28 +0800

    jsx support by web-mode

--- config/el-setting.el
+++ config/el-setting.el
@@ -326,6 +326,7 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 
 ;; [Y]asnippet------------------------------------------------------------>>
 (require 'yasnippet)
