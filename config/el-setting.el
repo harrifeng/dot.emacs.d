d587dd2606c06ec221f3e66ed1708a78657a1ffb
Haoran Feng <haoran.feng@alo7.com>
2016-11-23 16:00:57 +0800

    web-mode 2 indent

--- config/el-setting.el
+++ config/el-setting.el
@@ -321,6 +321,12 @@
 
 ;; [W]eb-mode------------------------------------------------------------->>
 (require 'web-mode)
+
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+
+
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
