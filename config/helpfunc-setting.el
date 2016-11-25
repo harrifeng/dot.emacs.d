6ae00f2937b42540c99bcb39c901eeeedc34585c
harri feng <harrifeng@gmail.com>
2016-11-25 22:57:33 +0800

    8123 for shadow socks

--- config/helpfunc-setting.el
+++ config/helpfunc-setting.el
@@ -101,13 +101,13 @@
 
 (defun local-proxy()
   (interactive)
-  (setq url-proxy-services '(("http" . "127.0.0.1:7777")))
+  (setq url-proxy-services '(("http" . "127.0.0.1:8123")))
   )
 
 ;; vagrant
 (defun vg-proxy()
   (interactive)
-  (setq url-proxy-services '(("http" . "10.0.2.2:7777")))
+  (setq url-proxy-services '(("http" . "10.0.2.2:8123")))
   )
 
 (defun no-proxy()
