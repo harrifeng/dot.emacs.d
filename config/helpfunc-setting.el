600de687a6ecd110bb6093b5b292f04f7aa67b3c
Haoran Feng <haoran.feng@alo7.com>
2016-12-09 10:15:40 +0800

    align by whitespace is the most useful function.
    If you do not want to align by whitespace, you can do as following
    
    1) selet the region
    2) C-u M-x align-regexp
    3) \(\s-*\) will show, you can append what you want like \(\s-*\)&
    4) RET to end

--- config/helpfunc-setting.el
+++ config/helpfunc-setting.el
@@ -426,3 +426,10 @@ minibuffer"
   "Find any non-ascii characters in the current buffer."
   (interactive)
   (occur "[^[:ascii:]]"))
+
+
+(defun align-whitespace (start end)
+  "Align columns by whitespace"
+  (interactive "r")
+  (align-regexp start end
+                "\\(\\s-*\\)\\s-" 1 0 t))
