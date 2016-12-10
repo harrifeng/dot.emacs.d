be45cb5fdb7aa2513b80059e96745c6d06c5aa7f
Harri Feng <harrifeng@gmail.com>
2016-12-10 23:16:02 +0800

    make linux font works

--- config/sub/sub-linux-font.el
+++ config/sub/sub-linux-font.el
@@ -2,3 +2,9 @@
 
 ;; Setting English Font
 ;; on Ubuntu need install font: sudo apt-get install -y fonts-inconsolata
+
+(qiang-set-font
+ '("Consolas" "Courier10 BT" "Courier New" "Monaco" "Inconsolata" "Menlo"  "DejaVu Sans Mono" "Monospace")
+ my-font-size
+ '("Microsoft Yahei" "新宋体" "KaiTi""楷体" "kai" "文泉驿等宽微米黑"
+   "黑体" ))
