abae67403a1d60063e4891d3e678c704a2e78f9d
Haoran Feng <haoran.feng@alo7.com>
2017-01-12 11:34:56 +0800

    Set font from panel and M-x describe-font to find the name for the font, and set in sub-xx-font.el

--- config/sub/sub-mac-font.el
+++ config/sub/sub-mac-font.el
@@ -5,5 +5,5 @@
      '("SF Mono" "Courier" "Fira Mono for Powerline" "Menlo" "Consolas" "Courier New" "Andale Mono" "Monaco" "Courier10 BT" "PT Mono"
         "DejaVu Sans Mono" "Monospace")
      my-font-size
-     '("Yuanti SC" "楷体" "kai" "Microsoft Yahei" "文泉驿等宽微米黑"
+     '("HanziPen SC" "Yuanti SC" "楷体" "kai" "Microsoft Yahei" "文泉驿等宽微米黑"
        "黑体" "新宋体" )))
