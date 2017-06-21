9e9f7ffe773c40607f0eb67311b8fb6b92fef8c0
Haoran Feng <haoran.feng@alo7.com>
2017-06-21 16:00:55 +0800

    new visible mark ring

--- config/tmp-setting.el
+++ config/tmp-setting.el
@@ -172,6 +172,14 @@
 (add-to-list 'load-path (concat my-lisps-path "nopackage"))
 (require 'livedown)
 (require 'redis-cli)
+(require 'visible-mark)
+(global-visible-mark-mode 1) ;; or add (visible-mark-mode) to specific hooks
+(defface visible-mark-active ;; put this before (require 'visible-mark)
+  '((((type tty) (class mono)))
+    (t (:background "magenta"))) "")
+(setq visible-mark-max 2)
+(setq visible-mark-faces `(visible-mark-face1 visible-mark-face2))
+
 (defun refresh-file ()
   "Revert buffer Only when it's really changed, maybe usefull"
   (interactive)
