f077dc5bd4fa1e2eaf14ff521b6898e0f1446e6e
Haoran Feng <haoran.feng@alo7.com>
2017-07-10 10:26:48 +0800

    linux term can not use this setting

--- config/sub/sub-linux-font.el
+++ config/sub/sub-linux-font.el
@@ -3,8 +3,8 @@
 ;; Setting English Font
 ;; on Ubuntu need install font: sudo apt-get install -y fonts-inconsolata
 
-(qiang-set-font
- '("Consolas" "Courier 10  Pitch" "Courier New" "Monaco" "Fira Mono" "Inconsolata" "Menlo"  "DejaVu Sans Mono" "Monospace")
- my-font-size
- '("Microsoft Yahei" "新宋体" "KaiTi""楷体" "kai" "文泉驿等宽微米黑"
-   "黑体" ))
+;; (qiang-set-font
+;;  '("Consolas" "Courier 10  Pitch" "Courier New" "Monaco" "Fira Mono" "Inconsolata" "Menlo"  "DejaVu Sans Mono" "Monospace")
+;;  my-font-size
+;;  '("Microsoft Yahei" "新宋体" "KaiTi""楷体" "kai" "文泉驿等宽微米黑"
+;;    "黑体" ))
