b165fd4ada7db0073220387bbcf96c1f4fcceef3
fenghaoran <harrifeng@gmail.com>
2017-11-30 10:19:03 +0800

    new font

--- config/sub/sub-nt-font.el
+++ config/sub/sub-nt-font.el
@@ -1,7 +1,7 @@
 (provide 'sub-nt-font)
 
 (qiang-set-font
- '("Consolas" "Courier10 BT" "Courier New" "Monaco" "Inconsolata" "Menlo"  "DejaVu Sans Mono" "Monospace")
+ '("Source Code Pro" "Consolas" "Courier10 BT" "Courier New" "Monaco" "Inconsolata" "Menlo"  "DejaVu Sans Mono" "Monospace")
  my-font-size
- '("微软雅黑" "Microsoft Yahei" "新宋体" "KaiTi""楷体" "kai" "文泉驿等宽微米黑"
+ '("新宋体" "楷体" "微软雅黑" "Microsoft Yahei" "KaiTi" "楷体" "kai" "文泉驿等宽微米黑"
    "黑体" ))
