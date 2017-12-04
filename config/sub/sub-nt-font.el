b43805b6970a5d92cc69be89b0f0af418b91c3f9
fenghaoran <harrifeng@gmail.com>
2017-12-04 21:05:14 +0800

    Use the english name is better

--- config/sub/sub-nt-font.el
+++ config/sub/sub-nt-font.el
@@ -3,5 +3,4 @@
 (qiang-set-font
  '("Source Code Pro" "Consolas" "Courier10 BT" "Courier New" "Monaco" "Inconsolata" "Menlo"  "DejaVu Sans Mono" "Monospace")
  my-font-size
- '("新宋体" "楷体" "微软雅黑" "Microsoft Yahei" "KaiTi" "楷体" "kai" "文泉驿等宽微米黑"
-   "黑体" ))
+ '("Microsoft Yahei" "KaiTi"))
