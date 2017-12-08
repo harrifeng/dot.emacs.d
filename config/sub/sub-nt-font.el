7006dda56d79a67d6f9498ee83fa8732ecd5a7b2
fenghaoran <harrifeng@gmail.com>
2017-12-08 09:32:01 +0800

    Inziu is clear, use same font size for english and chinese

--- config/sub/sub-nt-font.el
+++ config/sub/sub-nt-font.el
@@ -3,6 +3,5 @@
 (qiang-set-font
  '("Inziu Iosevka SC" "Courier10 BT" "Menlo" "Monaco" "Source Code Pro" "Inconsolata" "Consolas" "Courier New" "DejaVu Sans Mono" "Monospace")
  my-english-font-size
- '("KaiTi" "SimSun" "Microsoft Yahei")
- my-chinese-font-size
+ '("Inziu Iosevka SC" "KaiTi" "SimSun" "Microsoft Yahei")
  )
