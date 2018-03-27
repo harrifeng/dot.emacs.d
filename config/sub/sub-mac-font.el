ff81dbcf5faca2786bec81f7a578ab3315399ce5
Haoran Feng <harrifeng@gmail.com>
2018-03-27 09:39:58 +0800

    new font ok

--- config/sub/sub-mac-font.el
+++ config/sub/sub-mac-font.el
@@ -4,6 +4,6 @@
     (qiang-set-font
      '("Source Code Pro" "SF Mono" "Courier" "Fira Mono for Powerline" "Menlo" "Consolas" "Courier New" "Andale Mono" "Monaco" "Courier10 BT" "PT Mono"
         "DejaVu Sans Mono" "Monospace")
-     my-font-size
+     my-english-font-size
      '("Yuppy SC" "HanziPen SC" "Yuanti SC" "楷体" "kai" "Microsoft Yahei" "文泉驿等宽微米黑"
        "黑体" "新宋体" )))
