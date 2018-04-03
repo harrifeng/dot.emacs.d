54034b97cfb910c98ad402861e918244a767004a
Haoran Feng <harrifeng@gmail.com>
2018-04-03 13:22:58 +0800

    font from brew

--- config/sub/sub-mac-font.el
+++ config/sub/sub-mac-font.el
@@ -2,7 +2,7 @@
 
 (if (eq window-system 'ns)
     (qiang-set-font
-     '("Source Code Pro" "Fira Mono for Powerline" "Monaco" "SF Mono" "Menlo" "Consolas" "Courier New" "Andale Mono" "Courier10 BT" "PT Mono" "Courier" "DejaVu Sans Mono" "Monospace")
+     '("Source Code Pro for powerline" "Fira Mono for Powerline" "Monaco" "SF Mono" "Menlo" "Consolas" "Courier New" "Andale Mono" "Courier10 BT" "PT Mono" "Courier" "DejaVu Sans Mono" "Monospace")
      my-english-font-size
      '("Yuppy SC" "HanziPen SC" "Yuanti SC" "楷体" "kai" "Microsoft Yahei" "文泉驿等宽微米黑"
        "黑体" "新宋体" )))
