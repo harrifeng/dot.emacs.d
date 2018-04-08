040c1f1dd6350065e5ae0d63d8c972c44d40685f
Haoran Feng <harrifeng@gmail.com>
2018-04-07 21:12:59 -0700

    new setting for windows

--- config/sub/sub-nt-font.el
+++ config/sub/sub-nt-font.el
@@ -1,7 +1,7 @@
 (provide 'sub-nt-font)
 
 (qiang-set-font
- '("Inziu Iosevka SC" "Courier10 BT" "Menlo" "Monaco" "Source Code Pro" "Inconsolata" "Consolas" "Courier New" "DejaVu Sans Mono" "Monospace")
+ '("Source Code Pro" "Inziu Iosevka SC" "Courier10 BT" "Menlo" "Monaco" "Inconsolata" "Consolas" "Courier New" "DejaVu Sans Mono" "Monospace")
  my-english-font-size
  '("Inziu Iosevka SC" "KaiTi" "SimSun" "Microsoft Yahei")
  )
